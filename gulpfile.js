1bd7da03d4745e338dcb75592b5c77e4279a52f6
Vladislav <vlad@bannikon.ru>
2022-08-25 16:52:06 +0300

    start

---
+++ gulpfile.js
@@ -0,0 +1,68 @@
+// const gulp        = require('gulp');
+// const browserSync = require('browser-sync');
+// const sass = require('gulp-sass')(require('sass'));
+// const cleanCSS = require('gulp-clean-css');
+// const autoprefixer = require('gulp-autoprefixer');
+// const rename = require("gulp-rename");
+
+// gulp.task('server', function() {
+
+//     browserSync({
+//         server: {
+//             baseDir: "src"
+//         }
+//     });
+
+//     gulp.watch("src/*.html").on('change', browserSync.reload);
+// });
+
+// gulp.task('styles', function() {
+//     return gulp.src("src/sass/**/*.+(scss|sass)")
+//         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+//         .pipe(rename({suffix: '.min', prefix: ''}))
+//         .pipe(autoprefixer())
+//         .pipe(cleanCSS({compatibility: 'ie8'}))
+//         .pipe(gulp.dest("src/css"))
+//         .pipe(browserSync.stream());
+// });
+
+// gulp.task('watch', function() {
+//     gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel('styles'));
+// })
+// 
+// gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
+
+const { src, dest, parallel, series, watch, on } = require('gulp');
+const browserSync  = require('browser-sync');
+const sass         = require('gulp-sass')(require('sass'));
+const cleanCSS     = require('gulp-clean-css');
+const autoprefixer = require('gulp-autoprefixer');
+const rename       = require("gulp-rename");
+
+function server() {
+    browserSync.init({
+        server: {
+            baseDir: "src"
+        }
+    });
+};
+
+function styles() {
+    return src("src/sass/**/*.+(scss|sass)")
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(rename({suffix: '.min', prefix: ''}))
+        .pipe(autoprefixer())
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(dest("src/css"))
+        .pipe(browserSync.stream());
+};
+
+function startwatch() {
+    watch("src/sass/**/*.+(scss|sass)", styles);
+    watch("src/*.html").on('change', browserSync.reload);
+}
+
+exports.server  = server;
+exports.styles  = styles;
+
+exports.default = parallel(styles, server, startwatch);     

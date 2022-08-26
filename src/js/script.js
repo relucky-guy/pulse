4a46a444507125565365409489166478772a8089
Vladislav <vlad@bannikon.ru>
2022-08-27 00:44:34 +0300

    add slisk slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,18 @@
 $(document).ready(function(){
     $('.carousel__inner').slick({
-        dots: true,
-        infinite: true,
-        speed: 300,
-        slidesToShow: 1,
-        adaptiveHeight: true
+        speed: 1200,
+        // adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev" style="outline:none;"> <img src="icons/slider/chevron-left-solid.png" alt="left"></button>',
+        nextArrow: '<button type="button" type="button" class="slick-next" style="outline:none;"> <img src="icons/slider/right.svg" alt="left"></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                  dots: true,
+                  arrows: false,
+
+                }
+            }
+        ]
       });
-  });
+  }); 

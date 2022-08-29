7a1e9cbc39e5c491f1e856c6820dcfb977aa8509
Vladislav <vlad@bannikon.ru>
2022-08-29 19:08:50 +0300

    minors edits

--- src/mailer/smart.php
+++ src/mailer/smart.php
@@ -11,10 +11,10 @@ $mail->CharSet = 'utf-8';
 // $mail->SMTPDebug = 3;                               // Enable verbose debug output
 
 $mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.yandex.com';  // Specify main and backup SMTP servers
+$mail->Host = 'smtp.yandex.ru';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
 $mail->Username = 'left@bannikon.ru';                 // Наш логин
-$mail->Password = 'ocljnhegdmxqisjg';                           // Наш пароль от ящика
+$mail->Password = '';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
